060dfe53acbc4fcd13e938db5ab6d1ca9e843004
Casey Watts <casey.s.watts@gmail.com>
2018-08-23 09:11:58 -0400

    add another test; rename the test root file

--- __tests__/all-tests-test.js
+++ __tests__/all-tests-test.js
@@ -3,3 +3,4 @@ const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 defineTest(__dirname, 'transform', null, 'simple-replace');
 defineTest(__dirname, 'transform', null, 'leave-mirage-import-intact');
 defineTest(__dirname, 'transform', null, 'insert-into-existing-faker-import');
+// defineTest(__dirname, 'transform', null, 'irrelevant-parenthesis-issue');
